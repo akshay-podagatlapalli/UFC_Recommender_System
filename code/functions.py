d70d2ec1330acb2d018ceb19b130f378d7622b10
Akshay <akshay.podagatlapalli@gmail.com>
2023-06-18 16:43:47 -0400

    housekeeping

--- code/functions.py
+++ code/functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import ipywidgets as widgets
 from IPython.display import display, clear_output, Javascript, HTML
+import random
 
 
 def handle_likert_scale1_change(change):
@@ -19,6 +20,19 @@ def handle_likert_scale5_change(change):
     responses['question5'] = change.new
 
 
+def run():
+    button = widgets.Button(description='Run Cells Below')
+    output = widgets.Output()
+
+    def run_all_cells(button):
+        with output:
+            clear_output(wait=True)  # Clear previous output
+            display(Javascript('IPython.notebook.execute_cells_below()'))
+
+    button.on_click(run_all_cells)
+
+    display(button, output)
+
 def create_button_widget():
     button = widgets.Button(description='See Results')
     output = widgets.Output()
@@ -31,25 +45,3 @@ def create_button_widget():
     button.on_click(run_all_cells)
 
     display(button, output)
-
-def toggle_code_show():
-    code_show = True
-
-    html_code = '''
-        <script>
-        function code_toggle() {
-            if (code_show) {
-                $('div.input').hide();
-            } else {
-                $('div.input').show();
-            }
-            code_show = !code_show;
-        }
-        $(document).ready(code_toggle);
-        </script>
-        <form action="javascript:code_toggle()">
-            <input type="submit" value="Click here to toggle on/off the raw code.">
-        </form>
-    '''
-
-    display(HTML(html_code))
